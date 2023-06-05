496a591639915d1244d0853a072470c8cdde153b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-05 22:13:44 +0530

    starter

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'normalize.css';
+
+const App:React.FC = () => {
+  return (
+    <div className="App">
+      <h2>First text</h2>
+    </div>
+  );
+}
+
+export default App;
