7cc244d2a2203e228886b4975a044c59f4f510f0
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-06 03:44:48 +0530

    adding image

--- src/components/LandingMain.tsx
+++ src/components/LandingMain.tsx
@@ -2,16 +2,18 @@ import Pizza from "../assets/pizza.jpg";
 
 const LandingMain = () => {
   return (
-    <div className="w-full h-[32rem] grid grid-cols-2 ">
-      <div className="info">
+    <div className="w-full h-[32rem] grid grid-cols-2  ">
+      <div className="info flex flex-col justify-center items-center">
         <div>
-          <span>MOREE...</span>
-          <h2>THAN JUST PIZZA</h2>
+          <span className=" text-[--primary-blue] text-[8rem] font-bold ">
+            MOREE...
+          </span>
+          <h2 className="text-6xl text-left font-bold">THAN JUST PIZZA</h2>
         </div>
         <div>
-          <h3>
-            OUR MENU HAS SOMETHING FOR EVERYONE. SOP BY FOR A QUICK BITE OR GET
-            YOUR FAVORITES DELIVERED RIGHT TO YOU DOOR{" "}
+          <h3 className="px-20 mt-4 ml-4 text-left text-2xl">
+            OUR MENU HAS SOMETHING FOR EVERYONE. STOP BY FOR A QUICK BITE OR GET
+            YOUR FAVORITES DELIVERED RIGHT TO YOU DOOR
           </h3>
         </div>
       </div>
--- src/components/NavbarMain.tsx
+++ src/components/NavbarMain.tsx
@@ -3,7 +3,7 @@ import { BiUserCircle } from "react-icons/bi";
 const NavbarMain = () => {
   return (
     <div className="h-auto py-3 px-20 flex justify-between bg-[--primary-blue]">
-      <div className="font-bold text-2xl text-white">Yummy Slice Pizza</div>
+      <div className="font-bold text-2xl text-white cursor-pointer">Yummy Slice Pizza</div>
       <div className="w-1/4 flex justify-end px-4 border-l-2 border-secondary-500 cursor-pointer">
         <BiUserCircle className="text-4xl text-white" />
         <div className=" text-white ">
