6cd85a940dd34f7d5e63c1a914608f9aab85096f
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-07 23:44:51 +0530

    some corrections

--- src/components/LandingMain.tsx
+++ src/components/LandingMain.tsx
@@ -2,23 +2,23 @@ import Pizza from "../assets/pizza.jpg";
 
 const LandingMain = () => {
   return (
-    <div className="w-full h-[32rem] grid grid-cols-2  ">
+    <div className="w-full h-auto md:grid grid-cols-2  ">
       <div className="info flex flex-col justify-center items-center">
-        <div>
-          <span className=" text-[--primary-blue] text-[8rem] font-bold ">
+        <div className="my-5">
+          <span className=" lg:text-[7rem]  text-[--primary-blue] text-[6rem] font-bold ">
             MOREE...
           </span>
-          <h2 className="text-6xl text-left font-bold">THAN JUST PIZZA</h2>
+          <h2 className=" lg:text-5xl duration-300 text-4xl text-justify font-bold">THAN JUST PIZZA</h2>
         </div>
-        <div>
-          <h3 className="px-20 mt-4 ml-4 text-left text-2xl">
+        <div className="flex justify-center">
+          <h3 className="w-[90%] mb-3  text-justify text-xl md:text-2xl font-bold">
             OUR MENU HAS SOMETHING FOR EVERYONE. STOP BY FOR A QUICK BITE OR GET
             YOUR FAVORITES DELIVERED RIGHT TO YOU DOOR
           </h3>
         </div>
       </div>
-      <div>
-        <img className="h-full " src={Pizza} alt="pizza picture" />
+      <div className="md:w-full flex justify-center ">
+        <img className="h-auto md:w-full object-cover w-[90%]" src={Pizza} alt="pizza picture" />
       </div>
     </div>
   );
--- src/components/NavbarMain.tsx
+++ src/components/NavbarMain.tsx
@@ -2,13 +2,17 @@ import { BiUserCircle } from "react-icons/bi";
 
 const NavbarMain = () => {
   return (
-    <div className="h-auto py-3 px-20 flex justify-between bg-[--primary-blue]">
-      <div className="font-bold text-2xl text-white cursor-pointer">Yummy Slice Pizza</div>
-      <div className="w-1/4 flex justify-end px-4 border-l-2 border-secondary-500 cursor-pointer">
-        <BiUserCircle className="text-4xl text-white" />
-        <div className=" text-white ">
-          <h3>MY ACCOUNT</h3>
-          <h3>Log In / Sign Up</h3>
+    <div className="h-auto py-3 w-full flex bg-[--primary-blue]">
+      <div className="w-[1240px] mx-auto flex justify-between ">
+        <div className="font-bold text-2xl mx-2 text-white cursor-pointer">
+          Yummy Slice Pizza
+        </div>
+        <div className=" flex justify-end mx-2  border-secondary-500 cursor-pointer">
+          <BiUserCircle className=" text-3xl md:text-4xl text-white" />
+          <div className=" text-white ml-2 ">
+            <h3 className="text-xs md:text-sm">MY ACCOUNT</h3>
+            <h3 className="text-xs md:text-sm">Log In / Sign Up</h3>
+          </div>
         </div>
       </div>
     </div>
