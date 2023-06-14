e4b4758066d2c89e6c73c0f349ce23580730f4be
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-14 06:46:04 +0530

    adding customize button in product

--- src/components/Dashboard/Product.tsx
+++ src/components/Dashboard/Product.tsx
@@ -1,4 +1,5 @@
 import pizzaData from "../../utils/productsData/productsData";
+import { BiRightArrowAlt } from "react-icons/bi";
 
 interface dataType {
   name: string;
@@ -14,10 +15,15 @@ const Product: React.FC = () => {
         return (
           <section className=" h-72  rounded-md shadow-md flex flex-col justify-between">
             <div className=" relative">
-              <img className="rounded-t-md  " src={image} alt="pizza" />
-              <h2 className="text-white text-lg font-bold bg-blue-500 tracking-widest absolute bottom-1 ml-1 border-2  rounded-md px-1 border-white z-10">
-                Rs {price}
-              </h2>
+              <img className="rounded-t-md " src={image} alt="pizza" />
+              <div className=" flex justify-between items-center">
+                <h2 className="text-white font-bold bg-blue-500 tracking-widest absolute bottom-1 ml-1 border-2 rounded-md px-1 border-white z-10">
+                  Rs {price}
+                </h2>
+                <button className="text-gray-600 text-sm bg-white border border-white capitalize absolute right-1 bottom-1 px-1 rounded-md tracking-widest flex justify-center items-center">
+                  customize <BiRightArrowAlt className="text-lg" />
+                </button>
+              </div>
             </div>
             <div className="">
               <h2 className="capitalize text-xl font-semibold tracking-widest px-1">
@@ -28,7 +34,7 @@ const Product: React.FC = () => {
               </p>
             </div>
             <div className="flex justify-end py-2 ">
-              <button className="text-green-600 capitalize text-md border-2 px-2 py-1 rounded-md border-green-600 tracking-wide mr-2">
+              <button className="text-green-600 capitalize text-sm border-2 px-2 py-1 rounded-md border-green-600 tracking-widest mr-2">
                 Add To Cart
               </button>
             </div>
